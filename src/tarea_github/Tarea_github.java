2acd509a7821a785d3cdb2f0dbcc7e8478d2c6b1
Luis Villatoro <Luis Villatoro@LuisVillatoro27>
2025-08-11 23:50:33 -0600

    "Tarea de Github"

---
+++ src/tarea_github/Tarea_github.java
@@ -0,0 +1,21 @@
+package tarea_github;
+import java.util.Scanner;
+public class Tarea_github {
+    public static void main(String[] args) {
+        System.out.println("Ingrese un numero: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt() ;
+         if (a<10)    { 
+        System.out.println("El numero es de una cifra");
+        }
+        else if (a>9 && a <100) {
+        System.out.println("El numero es de dos cifras");
+        }
+        else if(a<1000){
+        System.out.println("El numero es de tres cifras");
+        }
+        else {
+        System.err.println("Error: El numero es invalido");
+        }
+    }   
+}
